85ed92a598b85e9a68b8c12a087f8a2a30cfcf6a
MaylinC <maylin.catherine.c@gmail.com>
2021-06-10 21:23:42 +0700

    Done GET, HEAD, and others

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Project-2-WebServer
+
+1. https://stackoverflow.com/questions/1442116/how-to-get-the-date-and-time-values-in-a-c-program
--- src/icws.c
+++ src/icws.c
@@ -11,6 +11,7 @@
 #include <netinet/in.h>
 #include <netinet/ip.h>
 #include <getopt.h>
+#include <time.h>
 #include "parse.h"
 #define MAXBUF 8192
 
@@ -28,6 +29,17 @@ static struct option long_options[] =   {
     {"root", required_argument, NULL, 'r'},
     {NULL, 0, NULL, 0}
 };
+
+
+char *local_time() {
+
+    char *date_time; 
+    time_t t = time(NULL);
+    struct tm *tm = localtime(&t);
+    date_time = asctime(tm); 
+    return date_time;
+
+}
 
 void write_logic(int data, int outputFd)
 {
@@ -58,19 +70,19 @@ void write_logic(int data, int outputFd)
     printf("DEBUG: Connection closed\n");
 }
 
+void respond_head(int connFd, char *uri, char *mime) { 
 
-void respond_all(int connFd, char *uri, char *mime)
-{
     char buf[MAXBUF];
     int uriFd = open(uri, O_RDONLY);
-    
-    char *msg = "404 Not Found";
-    if (uriFd < 0)
+
+    if (uriFd < 0 || mime == NULL)
     {
+        char *msg = "404 Not Found";
         sprintf(buf,
                 "HTTP/1.1 404 Not Found\r\n"
-                "Server: Micro\r\n"
-                "Connection: close\r\n\r\n");
+                "Date: %s\r\n"
+                "Server: icws\r\n"
+                "Connection: close\r\n\r\n",local_time());
         write_all(connFd, buf, strlen(buf));
         write_all(connFd, msg, strlen(msg));
         return;
@@ -79,21 +91,108 @@ void respond_all(int connFd, char *uri, char *mime)
     fstat(uriFd, &fstatbuf);
     sprintf(buf,
             "HTTP/1.1 200 OK\r\n"
-            "Server: Micro\r\n"
+            "Date: %s\r\n"
+            "Server: icws\r\n"
             "Connection: close\r\n"
             "Content-length: %lu\r\n"
             "Content-type: %s\r\n\r\n",
-            fstatbuf.st_size, mime);
+            local_time(),fstatbuf.st_size, mime);
+    write_all(connFd, buf, strlen(buf));  // write header into  connFd
+}
+
+
+void respond_all(int connFd, char *uri, char *mime)
+{
+    char buf[MAXBUF];
+    int uriFd = open(uri, O_RDONLY);
+    
+    char *msg = "404 Not Found";
+    if (uriFd < 0 || mime == NULL)
+    {
+        sprintf(buf,
+                "HTTP/1.1 404 Not Found\r\n"
+                "Date: %s\r\n"
+                "Server: icws\r\n"
+                "Connection: close\r\n\r\n",local_time());
+        write_all(connFd, buf, strlen(buf));
+        write_all(connFd, msg, strlen(msg));
+        return;
+    }
+
+    struct stat fstatbuf;
+    fstat(uriFd, &fstatbuf);
+    sprintf(buf,
+            "HTTP/1.1 200 OK\r\n"
+            "Date: %s\r\n"
+            "Server: icws\r\n"
+            "Connection: close\r\n"
+            "Content-length: %lu\r\n"
+            "Content-type: %s\r\n\r\n",
+            local_time(),fstatbuf.st_size, mime);
     write_all(connFd, buf, strlen(buf));  // write header into  connFd
     write_logic(uriFd, connFd); // send the content data into connFd
 }
 
+const char *get_filename_ext(const char *filename) {
+    const char *dot = strrchr(filename, '.');
+    if(!dot || dot == filename) return "";
+    return dot + 1;
+}
+
+char *parse_file_type(int connFd,char *rootFolder, Request *request) {
+
+    char *m_type; 
+
+    if (strcmp(get_filename_ext(request->http_uri),"html") == 0) {
+        
+        m_type = "text/html"; 
+        return m_type; 
+    }
+    else if (strcmp(get_filename_ext(request->http_uri),"jpg") == 0 || strcmp(get_filename_ext(request->http_uri),"jpeg") == 0) {
+        
+        m_type = "image/jpeg"; 
+        return m_type; 
+    }
+    else if (strcmp(get_filename_ext(request->http_uri),"css") == 0) {
+
+        m_type = "text/css"; 
+        return m_type; 
+    }
+    else if (strcmp(get_filename_ext(request->http_uri),"txt") == 0) {
+
+        m_type = "text/plain"; 
+        return m_type; 
+    }
+    else if (strcmp(get_filename_ext(request->http_uri),"js") == 0) {
+
+        m_type = "text/javascript"; 
+        return m_type; 
+    }
+    else if (strcmp(get_filename_ext(request->http_uri),"png") == 0) {
+                
+        m_type = "image/png"; 
+        return m_type; 
+        
+    }
+    else if (strcmp(get_filename_ext(request->http_uri),"gif") == 0) {
+        
+        m_type = "image/gif"; 
+        return m_type; 
+        
+    }
+    else {
+
+        return m_type;
+    }
+}
 
 void serve_http(int connFd,char *rootFolder)
 {
     
     char buf[MAXBUF];
     char buffer[MAXBUF]; 
+    char head[MAXBUF];
+    char newPath[80];
     int readRet; 
     int sizeRat = 0; 
     while((readRet = read_line(connFd, buf, MAXBUF))>0) {
@@ -108,42 +207,70 @@ void serve_http(int connFd,char *rootFolder)
 	    return;
 	}
 
-    // for(int i = 0; i < sizeRat; i++) {
-    //     printf("%c", buffer[i]); 
-    // }   
-
     Request *request = parse(buffer,sizeRat,connFd);
-
+    
     if (request == NULL) // handled malformede request
     {
+        printf("LOG: Bad Request \n");
+        sprintf(head,
+                "HTTP/1.1 400 Bad Request\r\n"
+                "Date: %s\r\n"
+                "Server: icws\r\n"
+                "Connection: close\r\n\r\n",local_time());
+        write_all(connFd, head, strlen(head));
         free(request);
-        exit(1);
+        return;
     }
 
-    char newPath[80];
-    if (strcasecmp(request->http_method, "GET") == 0)
+    else if (strcasecmp(request->http_method, "GET") == 0)
     {
-        if (request->http_uri[0] == '/')
-        {
+        if (request->http_uri[0] == '/') {
+
             sprintf(newPath, "%s%s", rootFolder, request->http_uri);
-            if (strstr(request->http_uri, "html") != NULL)
-            {
-                respond_all(connFd, newPath, "text/html");
-            }
-            else if (strstr(request->http_uri, "jpg") != NULL || strstr(request->http_uri, "jpeg") != NULL)
-            {
-                respond_all(connFd, newPath, "image/jpeg");
-            }
-            else
-            {
-                respond_all(connFd, newPath, NULL);
-            }
+
+            printf("newPath: %s \n", newPath); 
+            printf("exten: %s \n", get_filename_ext(request->http_uri)); 
+
+            respond_all(connFd, newPath, parse_file_type(connFd,rootFolder,request)); 
         }
     }
-    else
-    {
-        // respond_all(connFd, newPath, NULL);
+    else if (strcasecmp(request->http_method, "Head") == 0) {
+         
+        if (request->http_uri[0] == '/') {
+
+            sprintf(newPath, "%s%s", rootFolder, request->http_uri);
+
+            printf("newPath: %s \n", newPath); 
+            printf("exten: %s \n", get_filename_ext(request->http_uri)); 
+
+            respond_head(connFd, newPath, parse_file_type(connFd,rootFolder,request)); 
+        }
+    }
+
+    else if (strcasecmp(request->http_version, "HTTP/1.1") != 0) {
+        
+        printf("LOG: Not Supported the current HTTP Version \n");
+        sprintf(head,
+                "HTTP/1.1 505 HTTP Version Not Supported \r\n"
+                "Date: %s\r\n"
+                "Server: icws\r\n"
+                "Connection: close\r\n\r\n",local_time());
+        write_all(connFd, head, strlen(head));
+        free(request->headers); 
+        free(request);
+        return;
+    }
+
+    else {
         printf("LOG: Unknown request\n");
+        sprintf(head,
+                "HTTP/1.1 501 Method Unimplemented\r\n"
+                "Date: %s\r\n"
+                "Server: icws\r\n"
+                "Connection: close\r\n\r\n",local_time());
+        write_all(connFd, head, strlen(head));
+        free(request);
+        return;
     }
 }
 
